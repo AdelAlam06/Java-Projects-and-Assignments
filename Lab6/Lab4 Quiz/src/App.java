8d2df31493d9d72cae4cd731939c95de4803c693
AdelAlam06 <adel.alam2024@gmail.com>
2026-09-08 20:56:39 -0400

    Updated names

--- Lab6/Lab4 Quiz/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Student s = new Student("CPS209", "Ade", 18);
-        System.out.println(s.getAge());
-        System.out.println(s.getName());
-        System.out.println(s.getCourses());
-        s.add("CPS213");
-        System.out.println(s.getCourses());
-}   }
